1983aece2a3fddf136e9a97f4d258a98ce273602
rahul.rana <rahul.rana@instinctinnovations.com>
2025-09-06 15:00:49 +0530

    feat: setup of shared convex package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,23 @@
+"use client";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
+        <Button
+          onClick={() => {
+            addUser({ name: "Rahul Rana Bhau" });
+          }}
+        >
+          Add Rahul Bhau
+        </Button>
         <h1 className="text-2xl font-bold">Hello World from App</h1>
-        <Button size="sm">Button</Button>
+        {JSON.stringify(users)}
       </div>
     </div>
   );
---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,17 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const getMany = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("users").collect();
+  },
+});
+
+export const add = mutation({
+  args: { name: v.string() },
+  handler: async (ctx, args) => {
+    const id = await ctx.db.insert("users", { name: args.name });
+    return id;
+  },
+});
